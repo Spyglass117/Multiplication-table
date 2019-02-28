366fe37b0106c61cb9fb7af084ec6e3625b68e24
Spyglass117 <46973734+Spyglass117@users.noreply.github.com>
2019-02-28 14:43:29 -0500

    Updated from Brython Server: 2/28/2019 2:43:28 PM

--- multiplication-table.py
+++ multiplication-table.py
@@ -1,7 +1,7 @@
 """
 multiplication-table.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Me
+Credit: Walt, Mr. Dennison, A tutorial on github
 Assignment:
 
 Write and submit a Python program that prints a multiplication table. The user 
